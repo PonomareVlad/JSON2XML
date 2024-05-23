3240151790389414db407afc0d41c6c66600b491
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-05-23 03:47:02 +0200

    Prototype

--- api/index.mjs
+++ api/index.mjs
@@ -1,8 +1,14 @@
-export const config = {runtime: "edge"}
+import { toXML } from 'to-xml'
 
-export default async ({url, body, headers}) => new Response(JSON.stringify({
-    query: Object.fromEntries(new URL(url).searchParams.entries()),
-    headers: Object.fromEntries(headers.entries()),
-    body: await new Response(body).text(),
-    env: process.env,
-}))
+export const config = { runtime: 'edge' }
+
+const sanitizeURL = url => (url.startsWith('http') ? url : `http://${url}`)
+
+export const GET = async req => {
+  const url = new URL(sanitizeURL(req.url.slice(1)))
+  console.debug(url.href)
+  const response = await fetch(url)
+  const data = await response.json()
+  console.debug(data)
+  return new Response(toXML(data))
+}
