4f921ca4a54a16ac078650e0125b0b7de15de160
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-05-23 04:14:47 +0200

    Header

--- api/index.mjs
+++ api/index.mjs
@@ -2,10 +2,12 @@ import { toXML } from 'to-xml'
 
 export const config = { runtime: 'edge' }
 
-const sanitizeURL = url => (url.startsWith('http') ? url : `http://${url}`)
+const sanitizeURL = url => (url.startsWith('http') ? url : `https://${url}`)
 
 export default async req => {
   const { pathname, search } = new URL(req.url)
   const response = await fetch(sanitizeURL(pathname.slice(1) + search))
-  return new Response(toXML(await response.json()))
+  return new Response(toXML(await response.json()), {
+    headers: { 'content-type': 'text/xml' },
+  })
 }
