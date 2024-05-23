2c815fd24c241e7f08764d5fb1444b9c1c396a61
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-05-23 04:10:03 +0200

    Fix

--- api/index.mjs
+++ api/index.mjs
@@ -5,7 +5,7 @@ export const config = { runtime: 'edge' }
 const sanitizeURL = url => (url.startsWith('http') ? url : `http://${url}`)
 
 export default async req => {
-  const { pathname } = new URL(req.url)
-  const response = await fetch(sanitizeURL(pathname.slice(1)))
+  const { pathname, search } = new URL(req.url)
+  const response = await fetch(sanitizeURL(pathname.slice(1) + search))
   return new Response(toXML(await response.json()))
 }
