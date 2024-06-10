94a4cb0304ce3e4ac1cdcf26a9f2a606550a6353
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2024-06-10 12:54:53 +0200

    Update index.mjs

--- api/index.mjs
+++ api/index.mjs
@@ -7,7 +7,7 @@ const sanitizeURL = url => (url.startsWith('http') ? url : `https://${url}`)
 export default async req => {
   const { pathname, search } = new URL(req.url)
   const response = await fetch(sanitizeURL(pathname.slice(1) + search))
-  return new Response(toXML(await response.json()), {
+  return new Response(toXML({body: await response.json()}), {
     headers: { 'content-type': 'text/xml' },
   })
 }
